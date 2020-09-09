72b2023ca725ac8fa607f89c0c46fd71a6ee4688
argierdfj <argierincon@gmail.com>
2020-09-09 16:43:38 -0500

    añadiendo barra lateral

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Drawer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Drawer from '../components/Drawer';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Drawer
   }
 }
 </script>
