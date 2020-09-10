cb374b45224bd8cb118b58c804fa1eee05d68c33
argierdfj <argierincon@gmail.com>
2020-09-10 13:09:15 -0500

    componente idicadores

---
+++ src/components/GraphCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="graph">
+    <p>{{name}}</p>
+    <img src="../assets/img/Group 1135.png" alt="grafico1" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+  }
+};
+</script>
+
+<style>
+.graph {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 20%;
+  height: 220px;
+  background: #ffffff;
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.25);
+  border-radius: 25px;
+  font-size: 16px;
+  text-align: center;
+  font-weight: bold;
+}
+</style>
