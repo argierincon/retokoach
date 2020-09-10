3cfea27ad943b19311c9b6d99c68d5be70b45c30
argierdfj <argierincon@gmail.com>
2020-09-10 15:19:06 -0500

    componente resultados

---
+++ src/components/ResultsContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container-graph margin">
+    <TitleSection name="Resultados del Equipo" />
+    <div class="btn-search">
+      <MainBtn />
+      <Search />
+    </div>
+  </section>
+</template>
+
+<script>
+import TitleSection from "./TitleSection";
+import MainBtn from "../components/MainBtn";
+import Search from "../components/Search";
+
+export default {
+  components: {
+    TitleSection,
+    MainBtn,
+    Search,
+  },
+};
+</script>
+
+<style>
+.margin {
+  margin: 3% 0;
+}
+
+.btn-search {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
