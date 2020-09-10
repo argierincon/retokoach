5790e0602c1549f4e2626988e0a8fcdec77b4b52
argierdfj <argierincon@gmail.com>
2020-09-10 15:18:30 -0500

    componente filtro

---
+++ src/components/IndicatorsFilter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="indicators-filter">
+    <img src="../assets/img/filter.png" alt="filter" />
+    <p>Filtrar mis Indicadores</p>
+  </div>
+</template>
+
+<style>
+.indicators-filter {
+  display: flex;
+  align-items: center;
+  background: #fff;
+  width: 300px;
+  height: 40px;
+  border-radius: 25px;
+  padding: 2%;
+  cursor: pointer;
+}
+
+.indicators-filter > img {
+  width: 18px;
+  margin-right: 12px;
+}
+</style>
