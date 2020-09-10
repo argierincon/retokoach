a0e13fe1b162ea5842f5a9699a6738d0bd6c71bf
argierdfj <argierincon@gmail.com>
2020-09-10 15:18:51 -0500

    componente buscador

---
+++ src/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search">
+    <img src="../assets/img/search.png" alt="lupa" />
+    <input type="text" placeholder="Buscar" />
+  </div>
+</template>
+
+<style>
+.search {
+  display: flex;
+  align-items: center;
+  width: 300px;
+  height: 45px;
+  background: #ffffff !important;
+  border-radius: 25px;
+  padding: 1% 2%;
+}
+
+.search > img {
+  width: 10%;
+  margin-right: 10px;
+}
+
+.search > input {
+  outline: none;
+}
+</style>
