51fa91e225a9d7451496f3b1482a710c67377ab9
argierdfj <argierincon@gmail.com>
2020-09-10 13:09:33 -0500

    componente titulo de secciones

---
+++ src/components/TitleSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="title-section">{{name}}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+  }  
+}
+</script>
+
+<style>
+.title-section {
+  margin-bottom: 3%;
+  font-size: 28px;
+  font-weight: bold;
+  color: #ff7900;
+}
+</style>
