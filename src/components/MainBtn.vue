6003033c1cda0e5eec1feceecdeb0804b5fa9f17
argierdfj <argierincon@gmail.com>
2020-09-10 15:18:42 -0500

    componente boton principal

---
+++ src/components/MainBtn.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="custom-btn">
+    <img src="../assets/img/excel.png" alt="excel icon" />
+    <button>Generar Reporte</button>
+  </div>
+</template>
+
+<style>
+.custom-btn {
+  display: flex;
+  align-items: center;
+  height: 45px;
+  width: 200px;
+  padding: 1.3%;
+  background: linear-gradient(0deg, #ff7900, #ff7900);
+  border-radius: 25px;
+  margin-right: 4%;
+  cursor: pointer;
+  color: #fff;
+}
+
+.custom-btn > img {
+  width: 30px;
+  height: 30px;
+  margin-right: 8px;
+}
+
+.custom-btn > button {
+  outline: none;
+}
+
+</style>
